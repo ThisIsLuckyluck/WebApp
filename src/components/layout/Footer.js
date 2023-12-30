6249c501205f5030afa2b67bc90da52274dc4dc2
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-12-30 12:35:01 +0100

    create - footer.js

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <>
+            <section className="mx-auto max-w-6xl text-center text-lg font-bold py-10 grid sm:grid-cols-3 gap-0 text-white">
+                <div><p>Contact</p></div>
+                <div><p>Mention Légales</p></div>
+                <div><p>Nos pages</p></div>
+            </section>
+            <p className="text-center text-white">© Created by <Link href="https://github.com/StopThatTalace" className="font-bold">Talace</Link> - 2023</p>
+        </>
+    )
+}
