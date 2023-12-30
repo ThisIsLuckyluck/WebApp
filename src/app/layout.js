f671df8b6188eef00b7bebac11f629f0c760d789
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-12-30 12:34:24 +0100

    update - layout add footer Header Hero

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Roboto } from 'next/font/google'
 import './globals.css'
+import Footer from "@/components/layout/Footer";
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
@@ -9,9 +10,17 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+      <main className='bg-blue-950'>
+          <div className='max-w-6xl mx-auto p-4 relative'>
+              {children}
+          </div>
+          <div className="bg-black"><Footer /></div>
+      </main>
+      </body>
     </html>
   )
 }
