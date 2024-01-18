bd935f490af70bd2a2d5c13c040088adcf63e6be
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-01-18 22:23:46 +0100

    update - ui for card done

--- src/components/menu/card.jsx
+++ src/components/menu/card.jsx
@@ -1,14 +1,19 @@
 "use client"
+import Image from "next/image";
 
 export function CardUi() {
     return (
         <>
-        <section className={"bg-amber-100 rounded w-full max-w-52"}>
+        <section className={"bg-gray-300 rounded-lg w-full max-w-52 bg-opacity-30"}>
             <div className={"p-4 px-6"}>
-                <h1>Sushi</h1>
+                <p className={"text-3xl text-center text-white font-bold"}>10.99$</p>
+                <Image src={"/entree.jpg"} alt={"entree"} width={"1000"} height={"1000"} className={"py-2"}></Image>
                 <div>
-                    <p>Description</p>
-                    <button>ajouter au panier</button>
+                    <h1 className={"text-center font-bold text-white text-xl"}>item.name</h1>
+                    <div>
+                        <p className={"py-3 text-gray-200 text-sm italic"}>item.description</p>
+                        <button className={"bg-primary p-2 rounded text-sm font-bold text-white mx-auto w-full hover:bg-red-600"}>ajouter au panier</button>
+                    </div>
                 </div>
             </div>
         </section>
