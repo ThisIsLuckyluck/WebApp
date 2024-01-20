11e07f1da136143713bbbd0baba4124cf6df5955
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-01-20 22:36:43 +0100

    update - put the data in the card map the list

--- src/components/menu/card.jsx
+++ src/components/menu/card.jsx
@@ -1,5 +1,8 @@
-"use client"
+"use client";
 import Image from "next/image";
+import axios from "axios";
+import config from "@/conf";
+import { useEffect, useState } from "react";
 
 export function CardUi() {
     const [productData, setProductData] = useState(null);
@@ -23,19 +26,26 @@ export function CardUi() {
 
     return (
         <>
-        <section className={"bg-gray-300 rounded-lg w-full max-w-52 bg-opacity-30"}>
-            <div className={"p-4 px-6"}>
-                <p className={"text-3xl text-center text-white font-bold"}>10.99$</p>
-                <Image src={"/entree.jpg"} alt={"entree"} width={"1000"} height={"1000"} className={"py-2"}></Image>
-                <div>
-                    <h1 className={"text-center font-bold text-white text-xl"}>item.name</h1>
-                    <div>
-                        <p className={"py-3 text-gray-200 text-sm italic"}>item.description</p>
-                        <button className={"bg-primary p-2 rounded text-sm font-bold text-white mx-auto w-full hover:bg-red-600"}>ajouter au panier</button>
-                    </div>
-                </div>
+            <div className={"grid md:grid-cols-5 gap-2"}>
+                {productData && productData.map((item) => item.type_product === "appetizer" && (
+                    <section key={item.id_product} className={"min-w-52 bg-gray-300 rounded-lg w-full max-w-2xl bg-opacity-30"}>
+                        <div className={"p-4 px-6 w-full"}>
+                            <div>
+                                <p className={"text-3xl text-center text-white font-bold"}>{item.price}$</p>
+                                <Image src={"/entree.jpg"} alt={"img"} width={1000} height={1000} className={"py-2"} />
+                                <div className={"flex items-center justify-center w-full overflow-hidden"}>
+                                    <h1 className={"text-center font-bold text-white text-sm px-2"}>{item.product_name}</h1>
+                                    <p className={"italic text-gray-300 text-xs"}>{item.type_product}</p>
+                                </div>
+                                <div>
+                                    <p className={"py-3 text-gray-200 text-sm italic"}>{item.description}</p>
+                                    <button className={"bg-primary p-2 rounded text-sm font-bold text-white mx-auto w-full hover:bg-red-600"}>ajouter au panier</button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                ))}
             </div>
-        </section>
         </>
     );
 }
