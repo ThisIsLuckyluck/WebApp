0c6a3b6ef46ccef71579a4c28051895f1401a8e3
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-01-21 13:09:32 +0100

    update - starter Page, add pagination and compo separator

--- src/app/starter/page.jsx
+++ src/app/starter/page.jsx
@@ -1,10 +1,25 @@
 "use client"
 import {CardUi} from "@/components/menu/card";
+import { Separator } from "@/components/ui/separator"
+
 
 export default function starterPage(){
     return(
         <div>
-            <CardUi />
+            <div className={"flex items-center justify-between"}>
+                <h1 className={"text-2xl font-bold text-white pr-2"}>Entrée</h1>
+                <p className={"italic text-xs text-gray-400 sm:text-sm"}>Découvrez une expérience culinaire inoubliable
+                    dès la première bouchée.</p>
+                <div className={"flex text-gray-400 min-w-48 justify-between"}>
+                    <a href="/starter" className={"text-white hover:text-gray-200"}>Entree</a>
+                    <p>/</p>
+                    <a href="/plate" className={"hover:text-white"}>Plats</a>
+                    <p>/</p>
+                    <a href="/dessert" className={"hover:text-white"}>Desserts</a>
+                </div>
+            </div>
+            <Separator className={"mb-4 mt-2"}/>
+            <CardUi productType="appetizer"/>
         </div>
     )
 }
