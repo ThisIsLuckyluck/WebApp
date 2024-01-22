f20e8bc4d3c0171e11892061c8c4d8232f9bcb94
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-01-22 11:33:32 +0100

    add - new list of faq question

---
+++ src/app/contact/faqList.js
@@ -0,0 +1,52 @@
+export const faqList = [
+    {
+        name: "Creer Compte",
+        title: "Comment créer un compte?",
+        content: "Ouvrez l'application, cliquez sur 'S'inscrire' et remplissez les infos nécessaires. Confirmez votre compte via l'e-mail de confirmation."
+    },
+    {
+        name: "Reset Mot de Passe",
+        title: "Comment réinitialiser le mot de passe?",
+        content: "Sur la page de connexion, cliquez sur 'Mot de passe oublié'. Suivez les instructions envoyées par e-mail pour réinitialiser le mot de passe."
+    },
+    {
+        name: "Duree Livraison",
+        title: "Combien de temps pour la livraison?",
+        content: "Le temps de livraison dépend de votre emplacement. Consultez l'estimation lors de la confirmation de la commande."
+    },
+    {
+        name: "Modes de Paiement",
+        title: "Quels sont les modes de paiement?",
+        content: "Nous acceptons cartes de crédit, débit et paiements en ligne. Vérifiez les options disponibles lors du paiement."
+    },
+    {
+        name: "Commande Express",
+        title: "Comment commander expressément?",
+        content: "Choisissez 'Commande Express', sélectionnez vos articles, payez et récupérez rapidement au comptoir sushi."
+    },
+    {
+        name: "Options Personnalisees",
+        title: "Personnaliser ma commande?",
+        content: "Oui, personnalisez vos articles en utilisant 'Personnaliser' pour des demandes spéciales d'ajouts ou modifications."
+    },
+    {
+        name: "Suivi de Commande",
+        title: "Comment suivre ma commande?",
+        content: "Suivez le statut dans l'app. Soyez informé de la préparation, de l'emballage et de la disponibilité en temps réel."
+    },
+    {
+        name: "Carte de Fidelite",
+        title: "Programme de fidélité?",
+        content: "Enregistrez-vous, gagnez des points à chaque commande. Échangez-les contre réductions ou articles gratuits. Consultez 'Programme de fidélité' pour plus de détails."
+    },
+    {
+        name: "Assistance Client",
+        title: "Comment contacter l'assistance client?",
+        content: "Pour toute assistance, utilisez l'option 'Assistance Client' dans l'application ou contactez-nous à support@sushiapp.com."
+    },
+    {
+        name: "Avis Clients",
+        title: "Comment laisser un avis?",
+        content: "Partagez votre expérience en utilisant l'option 'Laisser un avis' dans l'application après avoir passé une commande."
+    },
+];
