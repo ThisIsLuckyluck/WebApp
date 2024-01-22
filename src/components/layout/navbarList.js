9086faceec966fdb093124ae3b518f02329ed844
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-01-22 15:10:14 +0100

    create - navbarList array

---
+++ src/components/layout/navbarList.js
@@ -0,0 +1,23 @@
+export const navbarList = [
+    {
+        title: "Nos entrées",
+        href: "/starter",
+        path: "/entree.jpg",
+        description:
+            "Bryan is in the kitchen ?",
+    },
+    {
+        title: "Nos plats",
+        href: "/plate",
+        path: "/Kerb_auth.png",
+        description:
+            "Okay bro",
+    },
+    {
+        title: "Nos desserts",
+        href: "/dessert",
+        path: "/LDAP_auth.png",
+        description:
+            "No way captain",
+    },
+]
