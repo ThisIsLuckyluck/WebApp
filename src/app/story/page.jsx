6543fd77fd44bd8fe702048f3c33daf9d969c47f
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-05 14:18:47 +0100

    update - assets and fix responsive story page

--- src/app/story/page.jsx
+++ src/app/story/page.jsx
@@ -13,11 +13,11 @@ import {
 
 export default function storyPage() {
     return (
-        <div className={"flex w-full items-center py-10 pb-32"}>
+        <div className={"py-3 md:flex w-full items-center md:py-10 pb-32"}>
             <section className={"w-full text-white"}>
                 <header className={"max-w-sm flex text-white items-center pb-10"}>
                     <Avatar className={"size-14 mr-3"}>
-                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarImage src="/cookMaster.png" />
                         <AvatarFallback>Makytori</AvatarFallback>
                     </Avatar>
                     <div>
@@ -27,6 +27,7 @@ export default function storyPage() {
                 </header>
                 <article>
                     <ScrollArea className="h-[400px] max-w-[550px] rounded">
+                        <div className={"max-w-[500px] mx-auto"}>
                         <h1 className={"text-sm font-bold py-2 sm:text-lg md:text-xl lg:text-2xl"}>Découverte de FastSushi</h1>
                         <p className={"text-xs py-2 sm:text-sm md:text-lg lg:text-xl"}>Bienvenue chez FastSushi, un établissement fondé en 2008 par le passionné de cuisine
                             japonaise, M. Makytori. Spécialisé dans la restauration rapide, FastSushi vous invite à un
@@ -44,15 +45,17 @@ export default function storyPage() {
                             expérience gastronomique unique qui ravira vos papilles.</p>
                         <h1 className={"text-sm font-bold py-2 sm:text-lg md:text-xl lg:text-2xl"}>À la Rencontre de M. Makytori</h1>
                         <p className={"text-xs py-2 sm:text-sm md:text-lg lg:text-xl"}>Rencontrez M. Makytori, le gérant passionné derrière l&apos;entreprise FastSushi. Fort d&apos;une expertise culinaire et d&apos;une passion pour la cuisine japonaise, M. Makytori a créé un lieu où l&apos;authenticité des saveurs rencontre l&apos;innovation culinaire. Son dévouement à faire découvrir la véritable essence de la cuisine japonaise se reflète dans chaque plat préparé chez FastSushi. Venez partager cette expérience gastronomique exceptionnelle et découvrez la vision unique de M. Makytori pour la cuisine japonaise à travers notre menu diversifié et savoureux.</p>
+                        </div>
                     </ScrollArea>
                 </article>
             </section>
             <section className={"w-full"}>
                 <Carousel className={"max-w-sm mx-auto"}>
-                    <CarouselContent>
+                    <CarouselContent className={"items-center"}>
+                        <CarouselItem><Image className={"rounded-sm"} src={"/storyAssets/sushi-oke.jpg"} width={"1000"} height={"1000"} alt={""}/></CarouselItem>
                         <CarouselItem><Image className={"rounded-sm"} src={"/entree.jpg"} width={"1000"} height={"1000"} alt={""}/></CarouselItem>
-                        <CarouselItem><Image className={"rounded-sm"} src={"/entree.jpg"} width={"1000"} height={"1000"} alt={""}/></CarouselItem>
-                        <CarouselItem><Image className={"rounded-sm"} src={"/entree.jpg"} width={"1000"} height={"1000"} alt={""}/></CarouselItem>
+                        <CarouselItem><Image className={"rounded-sm"} src={"/storyAssets/Cooking-class.jpg"} width={"1000"} height={"500"} alt={""}/></CarouselItem>
+                        <CarouselItem><Image className={"rounded-sm"} src={"/storyAssets/chief.png"} width={"1000"} height={"500"} alt={""}/></CarouselItem>
                     </CarouselContent>
                     <CarouselPrevious />
                     <CarouselNext />
