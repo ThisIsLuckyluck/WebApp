36a6bbaf0ef4675f1e34481a9f5862333c8644ee
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-06 12:53:00 +0100

    update - button hero

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,15 +6,15 @@ export default function Hero() {
         <section className="hero md:mt-4 text-white">
             <div className="py-8 md:py-12">
                 <h1 className="text-4xl font-semibold">
-                    Custom ton <span className="text-primary">Sushi </span>
+                    La maison du <span className="text-primary">Sushi </span>
                 </h1>
                 <p className="my-6 text-gray-300 text-base">
                     Le sushi est la pièce manquante qui rend chaque jour complet, une joie simple mais délicieuse dans la vie.
                 </p>
-                <div className="flex gap-4 text-sm">
+                <div className="flex gap-4 text-sm items-center">
                     <a href={"/starter"}>
                         <button
-                            className="flex justify-center bg-primary uppercase items-center gap-2 text-white px-4 py-2 rounded-md">
+                            className="flex justify-center bg-primary items-center gap-2 text-white px-4 py-2 rounded-md">
                             Commander
                         </button>
                     </a>
